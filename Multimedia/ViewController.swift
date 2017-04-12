72fdb9f2adf9e7a2a460941efc665096121414d4
Alumno <alumno@mbp-02.local>
2017-04-11 19:42:29 -0500

    Initial Commit

---
+++ Multimedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multimedia
+//
+//  Created by Alumno on 11/04/17.
+//  Copyright © 2017 Jorge Luis Limo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
